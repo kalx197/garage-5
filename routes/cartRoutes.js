ba068a04115505b7d9728234f84749c29d620091
kalx197 <kaleabteklemariam197@gmail.com>
2026-07-09 15:59:33 -0600

    Delete routes directory

--- routes/cartRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addToCart, checkoutCart, clearCart } = require('../controllers/cartController');
-
-router.post('/add', addToCart);
-router.post('/checkout', checkoutCart);
-router.post('/clear', clearCart);
-
-module.exports = router;
--- routes/inventoryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllStatus, updateStock } = require('../controllers/inventoryController');
-
-router.get('/status', getAllStatus);
-router.patch('/:id', updateStock);
-
-module.exports = router;
