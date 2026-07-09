0e8eb0a778e59a948163fd00efe40a177dba1389
kalx197 <kaleabteklemariam197@gmail.com>
2026-07-09 16:06:04 -0600

    Create authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied. You must be logged in as an Admin.' });
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid session token.' });
+  }
+};
